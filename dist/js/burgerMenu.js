7744dc0f14505ca54f6ffb745f3c2926a7f30ed0
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-05-19 18:31:28 -0500

    Finish About mobile version and burgermenu

---
+++ dist/js/burgerMenu.js
@@ -0,0 +1,30 @@
+const menuBtn = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+const menu = document.querySelector('.nav');
+const menuItems = document.querySelectorAll('.menu-item');
+
+let showMenu = false;
+
+
+const toggleMenu = () => {
+  if (!showMenu) {
+    menu.classList.add('showMenu')
+    hamburger.classList.add('open')
+    showMenu = true
+  }
+  
+  else {
+    menu.classList.remove('showMenu')
+    hamburger.classList.remove('open')
+    showMenu = false
+  }
+}
+
+menuBtn.addEventListener('click', toggleMenu);
+export default toggleMenu;
+
+menuItems.forEach(
+  (menuItem) => {
+    menuItem.addEventListener('click', toggleMenu);
+  },
+);
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,5 @@
 import populateCards from './featuredSpeakers.js';
+import toggleMenu from './burgerMenu.js';
 
 populateCards();
+toggleMenu();
