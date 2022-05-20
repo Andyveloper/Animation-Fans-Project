6f94eb32ca739a9eaa19cd655ed99851d13dd98a
Zeraltz <96924302+Zeraltz@users.noreply.github.com>
2022-05-19 19:53:05 -0500

    Finish about webpage completely

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -5,20 +5,17 @@ const menuItems = document.querySelectorAll('.menu-item');
 
 let showMenu = false;
 
-
 const toggleMenu = () => {
   if (!showMenu) {
-    menu.classList.add('showMenu')
-    hamburger.classList.add('open')
-    showMenu = true
+    menu.classList.add('showMenu');
+    hamburger.classList.add('open');
+    showMenu = true;
+  } else {
+    menu.classList.remove('showMenu');
+    hamburger.classList.remove('open');
+    showMenu = false;
   }
-  
-  else {
-    menu.classList.remove('showMenu')
-    hamburger.classList.remove('open')
-    showMenu = false
-  }
-}
+};
 
 menuBtn.addEventListener('click', toggleMenu);
 export default toggleMenu;
